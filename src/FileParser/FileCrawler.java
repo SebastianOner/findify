dfd626826b663180b7d4595d45c123ee22968759
Sebastian Oßner <ossner@in.tum.de>
2019-11-23 01:06:58 +0100

    changed FileReader to return list

---
+++ src/FileParser/FileCrawler.java
@@ -0,0 +1,11 @@
+package FileParser;
+
+public class FileCrawler {
+    private String projectPath;
+
+    public FileCrawler(String projectPath) {
+        this.projectPath = projectPath;
+    }
+
+
+}
