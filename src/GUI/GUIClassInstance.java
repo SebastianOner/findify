69d41cbf92362f8b40f0c5444fcfbbc778fe7980
Sebastian Oßner <github@sebastianoner.com>
2019-11-23 17:21:29 +0100

    changed box-choosing semantics

--- src/GUI/GUIClassInstance.java
+++ src/GUI/GUIClassInstance.java
@@ -2,42 +2,43 @@ package GUI;
 
 public class GUIClassInstance {
     //class types
-    public Boolean interfaceAtt=null, abstractClassAtt=null, ENUMAtt=null, finalAtt=null, standardClassAtt=null;
+    public Boolean interfaceAtt = null, abstractClassAtt = null, ENUMAtt = null, finalAtt = null, standardClassAtt = null;
     //modifiers
-    public Boolean publicAtt=null, privateAtt=null, protectedAtt=null, staticAtt=null;
+    public Boolean publicAtt = null, privateAtt = null, protectedAtt = null, staticAtt = null;
     //extended yes or nos
-    public Boolean extendedYes=null, extendedNo=null;
+    public Boolean extendedYes = null, extendedNo = null;
     //implemented yes or nos
-    public Boolean implementedYes=null, implementedNo=null;
+    public Boolean implementedYes = null, implementedNo = null;
     //generics yes or no
-    public Boolean genericsYes=null, genericsNo=null;
+    public Boolean genericsYes = null, genericsNo = null;
     //attribute types
-    public Boolean intAtt=null, stringAtt=null, arraysAtt=null, doubleAtt=null, booleanAtt=null, charAtt=null;
+    public Boolean intAtt = null, stringAtt = null, arraysAtt = null, doubleAtt = null, booleanAtt = null, charAtt = null;
     //possible class name
-    public String name=null;
+    public String name = null;
 
-    public String toString(){
+    public String toString() {
         String extend, implement, generic;
-        if(extendedYes.booleanValue() == true)
+        if (extendedYes) {
             extend = "Yes";
-        else if(extendedNo.booleanValue() == false)
+        } else if (!extendedNo) {
             extend = "No";
-        else
+        } else {
             extend = "Not specified";
-        if(implementedYes.booleanValue() == true)
+        }
+        if (implementedYes)
             implement = "Yes";
-        else if(implementedNo.booleanValue() == false)
+        else if (implementedNo.booleanValue() == false)
             implement = "No";
         else
             implement = "Not specified";
-        if(genericsYes.booleanValue() == true)
+        if (genericsYes.booleanValue() == true)
             generic = "Yes";
-        else if(genericsNo.booleanValue() == false)
+        else if (genericsNo.booleanValue() == false)
             generic = "No";
         else
             generic = "Not specified";
 
-        return  "Interface: " + interfaceAtt.toString() +
+        return "Interface: " + interfaceAtt.toString() +
                 "\nAbstract Class: " + abstractClassAtt.toString() +
                 "\nENUM: " + ENUMAtt.toString() +
                 "\nFinal: " + finalAtt.toString() +
