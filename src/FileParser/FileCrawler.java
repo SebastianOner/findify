664f74c48bcf111b030fae8fb858271a7d6f7592
Sebastian Oßner <ossner@in.tum.de>
2019-11-23 01:57:23 +0100

    Added fileCrawler

--- src/FileParser/FileCrawler.java
+++ src/FileParser/FileCrawler.java
@@ -1,11 +1,29 @@
 package FileParser;
 
-public class FileCrawler {
-    private String projectPath;
+import java.io.File;
 
-    public FileCrawler(String projectPath) {
-        this.projectPath = projectPath;
+public class FileCrawler {
+    /**
+     * crawler gets handed a path and finds all java files and hands their contents over to
+     * the parser, to get parsed into a class
+     *
+     * @param path: the path of the file/filder we want to look at
+     */
+    static void crawl(String path) {
+        if (path.endsWith(".java")) {
+            // TODO: 23.11.19 Hand over java file to fileParser
+            return;
+        }
+        File[] files = new File(path).listFiles();
+        if (files == null) {
+            return;
+        }
+        for (File f : files) {
+            crawl(f.getAbsolutePath());
+        }
     }
 
-
+    public static void main(String[] args) {
+        crawl("/home/sebastian/OneDrive/Repositories/findify/findify");
+    }
 }
