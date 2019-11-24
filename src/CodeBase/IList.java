db7ee0ab80031e93aa12a4fd5ad8e0dff4f7295a
DanielKruProgram <43455085+DanielKruProgram@users.noreply.github.com>
2019-11-24 08:32:20 +0100

    Reformatted

--- src/CodeBase/IList.java
+++ src/CodeBase/IList.java
@@ -5,14 +5,14 @@ import java.util.Iterator;
 /**
  * Interface for a simple List. Random access to all items in the list is provided.
  * The numbering of elements in the list begins at 0.
- *
  */
-public interface IList<E> extends Iterable<E>{
+public interface IList<E> extends Iterable<E> {
 
     /**
      * Add an item to the end of this list.
      * <br>pre: none
      * <br>post: size() = old size() + 1, get(size() - 1) = item
+     *
      * @param item the data to be added to the end of this list
      */
     void add(E item);
@@ -22,7 +22,8 @@ public interface IList<E> extends Iterable<E>{
      * <br>pre: 0 <= pos <= size()
      * <br>post: size() = old size() + 1, get(pos) = item, all elements in
      * the list with a positon >= pos have a position = old position + 1
-     * @param pos the position to insert the data at in the list
+     *
+     * @param pos  the position to insert the data at in the list
      * @param item the data to add to the list
      */
     void insert(int pos, E item);
@@ -33,7 +34,8 @@ public interface IList<E> extends Iterable<E>{
      * <br>pre: 0 <= pos < size()
      * <br>post: get(pos) = item, return the
      * old get(pos)
-     * @param pos the position in the list to overwrite
+     *
+     * @param pos  the position in the list to overwrite
      * @param item the new item that will overwrite the old item
      * @return the old data at the specified position
      */
@@ -43,6 +45,7 @@ public interface IList<E> extends Iterable<E>{
      * Get an element from the list.
      * <br>pre: 0 <= pos < size()
      * <br>post: return the item at pos
+     *
      * @param pos specifies which element to get
      * @return the element at the specified position in the list
      */
@@ -54,6 +57,7 @@ public interface IList<E> extends Iterable<E>{
      * <br>pre: 0 <= pos < size()
      * <br>post: size() = old size() - 1, all elements of
      * list with a positon > pos have a position = old position - 1
+     *
      * @param pos the position of the element to remove from the list
      * @return the data at position pos
      */
@@ -65,6 +69,7 @@ public interface IList<E> extends Iterable<E>{
      * <br>pre: none
      * <br>post: if obj is in this list the first occurence has been removed and size() = old size() - 1.
      * If obj is not present the list is not altered in any way.
+     *
      * @param obj The item to remove from this list.
      * @return Return <tt>true</tt> if this list changed as a result of this call, <tt>false</tt> otherwise.
      */
@@ -75,8 +80,9 @@ public interface IList<E> extends Iterable<E>{
      * This list is not changed as a result of this call.
      * <br>pre: <tt>0 <= start < size(), start <= stop <= size()</tt>
      * <br>post: return a list whose size is stop - start and contains the elements at positions start through stop - 1 in this list.
+     *
      * @param start index of the first element of the sublist.
-     * @param stop stop - 1 is the index of the last element of the sublist.
+     * @param stop  stop - 1 is the index of the last element of the sublist.
      * @return a list with <tt>stop - start</tt> elements, The elements are from positions <tt>start</tt> inclusive to
      * <tt>stop</tt> exclusive in this list.
      */
@@ -86,6 +92,7 @@ public interface IList<E> extends Iterable<E>{
      * Return the size of this list. In other words the number of elements in this list.
      * <br>pre: none
      * <br>post: return the number of items in this list
+     *
      * @return the number of items in this list
      */
     int size();
@@ -95,6 +102,7 @@ public interface IList<E> extends Iterable<E>{
      * <br>pre: none
      * <br>post: return the index of the first element equal to item
      * or -1 if item is not present
+     *
      * @param item the element to search for in the list
      * @return return the index of the first element equal to item or a -1 if item is not present
      */
@@ -105,8 +113,9 @@ public interface IList<E> extends Iterable<E>{
      * <br>pre: 0 <= pos < size()
      * <br>post: return the index of the first element equal to item starting at pos
      * or -1 if item is not present from position pos onward
+     *
      * @param item the element to search for in the list
-     * @param pos the position in the list to start searching from
+     * @param pos  the position in the list to start searching from
      * @return starting from the specified position return the index of the first element equal to item or a -1 if item is not present between pos and the end of the list
      */
     int indexOf(E item, int pos);
@@ -129,8 +138,9 @@ public interface IList<E> extends Iterable<E>{
      * Remove all elements in this list from <tt>start</tt> inclusive to <tt>stop</tt> exclusive.
      * <br>pre: <tt>0 <= start < size(), start <= stop <= size()</tt>
      * <br>post: <tt>size() = old size() - (stop - start)</tt>
+     *
      * @param start position at beginning of range of elements to be removed
-     * @param stop stop - 1 is the position at the end of the range of elements to be removed
+     * @param stop  stop - 1 is the position at the end of the range of elements to be removed
      */
     void removeRange(int start, int stop);
 
@@ -140,6 +150,7 @@ public interface IList<E> extends Iterable<E>{
      * are in order based on position in the
      * list with the first element
      * first. Adjacent elements are seperated by comma's
+     *
      * @return a String representation of this IList
      */
     public String toString();
@@ -148,6 +159,7 @@ public interface IList<E> extends Iterable<E>{
      * Determine if this IList is equal to other. Two
      * ILists are equal if they contain the same elements
      * in the same order.
+     *
      * @return true if this IList is equal to other, false otherwise
      */
     public boolean equals(Object other);
